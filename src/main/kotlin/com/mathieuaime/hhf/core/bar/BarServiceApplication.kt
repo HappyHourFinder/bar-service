b7f89db62ac740115fc0c87e06889e90b5bf9eba
Mathieu Aimé <aime.mathieu1@gmail.com>
2020-04-28 19:11:14 +0200

    enable discovery client

--- src/main/kotlin/com/mathieuaime/hhf/core/bar/BarServiceApplication.kt
+++ src/main/kotlin/com/mathieuaime/hhf/core/bar/BarServiceApplication.kt
@@ -2,8 +2,10 @@ package com.mathieuaime.hhf.core.bar
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 open class BarServiceApplication
 
 fun main(args: Array<String>) {
