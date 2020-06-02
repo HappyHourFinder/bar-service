8ad74bf0b30a190e400ed1df52391a724b791e61
Mathieu Aimé <aime.mathieu1@gmail.com>
2020-06-02 14:44:14 +0200

    set json content type for BarController

--- src/main/kotlin/com/mathieuaime/hhf/core/bar/controller/BarController.kt
+++ src/main/kotlin/com/mathieuaime/hhf/core/bar/controller/BarController.kt
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
-@RequestMapping("/bars")
+@RequestMapping("/bars", produces = ["application/json"])
 @RestController
 class BarController(private val barService: BarService) {
     @GetMapping
